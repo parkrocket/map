918957c834a1c0bfa81685d7210cea7f9893f4f6
parkrocket <shinbyro@kakao.kr>
2025-01-17 17:24:19 +0900

    geolaction

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // 정적 파일 생성
+  trailingSlash: true, // URL에 슬래시 추가 (정적 빌드 파일 구조에 필요)
+};
 
 export default nextConfig;
