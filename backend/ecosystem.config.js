4d5c8bed11114ef6c9a4d6c54b22c8344efd43d1
parkrocket <shinbyro@kakao.kr>
2025-01-17 17:32:17 +0900

    geolaction

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "frontend-app",
-      script: "npm",
-      args: "start",
-      cwd: "/home/ubuntu/frontend", // 작업 디렉토리
-      interpreter: "none",
+      name: "backend",
+      script: "./app.js", // 백엔드 서버의 진입점 파일
+      instances: 1, // 단일 인스턴스
+      autorestart: true, // 오류 발생 시 자동 재시작
+      watch: false, // 코드 변경 시 자동 재시작 비활성화
       env: {
-        NODE_ENV: "production", // Next.js 환경
+        NODE_ENV: "development", // 개발 환경 변수
+      },
+      env_production: {
+        NODE_ENV: "production", // 프로덕션 환경 변수
       },
     },
   ],
